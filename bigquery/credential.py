9c02ced7f594d0a3794d9040ae9b9df1f6fbf80a
Doris Lee <dorisjunglinlee@gmail.com>
2023-03-22 20:56:40 +0000

    01 working

--- bigquery/credential.py
+++
@@ -1,9 +0,0 @@
-params = {
-  "account": "<account_identifier>",
-  "user": "<username>",
-  "password": "<password>",
-  "role": "<role_name>",
-  "warehouse": "<warehouse_name>",
-  "database": "<database_name>",
-  "schema": "<schema_name>"
-}
