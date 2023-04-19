8a682da237d87b081e4c57122c3f7d1ba17cc74c
Doris Lee <dorisjunglinlee@gmail.com>
2023-04-20 04:21:53 +0800

    duckdb populate datasets and working tutorial 1

--- duckdb/populate_datasets.py
+++ duckdb/populate_datasets.py
@@ -1,17 +1,20 @@
-import ponder.duckdb
-import modin.pandas as pd
+import ponder
+ponder.init()
 
-duckdb_con = ponder.duckdb.connect("ponder.db", client_row_transfer_limit=1000000000,  read_only = False)
-ponder.duckdb.init(duckdb_con, client_row_transfer_limit=1000000000, timeout=60000)
+import duckdb
+duckdb_con = duckdb.connect("ponder.db")
+ponder.configure(default_connection=duckdb_con)
+
+import modin.pandas as pd
 
 df = pd.read_csv("https://github.com/ponder-org/ponder-datasets/blob/main/citibike_trial.csv?raw=True", on_bad_lines='skip')
 df.to_sql("PONDER_CITIBIKE",duckdb_con,index=False)
 print("Uploaded dataset to PONDER_CITIBIKE")
 
 
-# df = pd.read_csv("https://github.com/ponder-org/ponder-datasets/blob/main/books.csv?raw=True", on_bad_lines='skip')
-# df.to_sql("PONDER_BOOKS",duckdb_con,index=False)
-# print("Uploaded dataset to PONDER_BOOKS")
+df = pd.read_csv("https://github.com/ponder-org/ponder-datasets/blob/main/books.csv?raw=True", on_bad_lines='skip')
+df.to_sql("PONDER_BOOKS",duckdb_con,index=False)
+print("Uploaded dataset to PONDER_BOOKS")
 
 
 df = pd.read_csv("https://github.com/ponder-org/ponder-datasets/blob/main/yellow_tripdata_2015-01.csv?raw=True", on_bad_lines='skip')
@@ -34,3 +37,4 @@ print("Uploaded dataset to PONDER_PART")
 df = pd.read_csv("https://github.com/ponder-org/ponder-datasets/blob/main/tpch/supplier.csv?raw=True", on_bad_lines='skip')
 df.to_sql("PONDER_SUPPLIER",duckdb_con,index=False)
 print("Uploaded dataset to PONDER_SUPPLIER")
+duckdb_con.close()
