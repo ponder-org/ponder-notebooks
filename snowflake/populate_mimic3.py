3a30179c7107abaf4f9dd7d0dfa276960a41a773
Doris Lee <dorisjunglinlee@gmail.com>
2023-04-21 07:19:39 +0800

    add mimic3 script and prints

--- snowflake/populate_mimic3.py
+++ snowflake/populate_mimic3.py
@@ -36,9 +36,12 @@ import modin.pandas as pd
 
 tmp = pd.read_csv("https://raw.githubusercontent.com/ponder-org/ponder-datasets/main/mimic-iii/ICUSTAYS.csv", on_bad_lines='skip')
 tmp.to_sql("ICUSTAYS",snowflake_con,index=False)
+print("Uploaded dataset to ICUSTAYS")
 
 tmp = pd.read_csv("https://raw.githubusercontent.com/ponder-org/ponder-datasets/main/mimic-iii/PATIENTS.csv", on_bad_lines='skip')
 tmp.to_sql("PATIENTS",snowflake_con,index=False)
+print("Uploaded dataset to PATIENTS")
 
 tmp = pd.read_csv("https://raw.githubusercontent.com/ponder-org/ponder-datasets/main/mimic-iii/ADMISSIONS.csv", on_bad_lines='skip')
 tmp.to_sql("ADMISSIONS",snowflake_con,index=False)
+print("Uploaded dataset to ADMISSIONS")
